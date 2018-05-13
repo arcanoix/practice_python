a7f4fc8d3a3e9eed97a810ee35d474ff1527202b
thavoo <gustavoh.2312@gmail.com>
2018-05-13 14:33:40 -0400

    Update

---
+++ for.py
@@ -0,0 +1,3 @@
+#ciclo for en python
+for deporte in ["futbol","tenis","beisbol"]:
+	print("me gusta jugar al %s" %deporte)
---
+++ for2.py
@@ -0,0 +1,2 @@
+for numero in [1,2,3,4,5,6,7,8,9,10]:
+	print ("%d x 5 = %d" %(numero, numero*5))
